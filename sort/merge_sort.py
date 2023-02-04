78a23324c9dba56719bad2ab93119850e61589c3
talant_rahimberdiev <rahimberdiev240793@gmail.com>
2023-02-05 01:58:30 +0300

    asdf

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -1,14 +1,2 @@
-# Python program for implementation of MergeSort
 
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
-
-
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
-
-    # create temp arrays
-    L = [0] * (n1)
-    R = [0] * (n2)
+alfa=1993
