b2ff7a2c3523696a9c1f396db3432a420f5fa93d
talant_rahimberdiev <rahimberdiev240793@gmail.com>
2023-02-05 00:28:44 +0300

    structural_patterns

---
+++ structural_patterns/facade.py
@@ -0,0 +1,26 @@
+
+class Washing:
+   def wash(self):
+      print('washes...')
+
+class Spinning:
+   def spin(self):
+      print('spines...')
+
+class Rinsing:
+   def rinse(self):
+      print('rinses...')
+
+class Facade_machine:
+   def __init__(self):
+      self.washing=Washing()
+      self.spinning=Spinning()
+      self.rinsing=Rinsing()
+   
+   def work(self):
+      self.washing.wash()
+      self.spinning.spin()
+      self.rinsing.rinse()
+
+washing_machine=Facade_machine()
+washing_machine.work()
